6f2dd649df4818163f26dc7903c9db12bc457b73
Febdk <febydiky@gmail.com>
2026-03-24 15:46:22 +0700

    home

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -18,7 +18,10 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16 md:h-20">
           {/* Logo */}
-          <div className="flex items-center">
+          <div 
+            className="flex items-center"
+            onClick={() => scrollToSection('home')}
+          >
             <img src={logo} alt="Emoon Logo" className="h-8 md:h-10" />
           </div>
 
